bc14e898fbe4b414d03f7fd2ac83cc8dfbbbca36
Antoine Vidal-Mazuy <antoine.vidalma@gmail.com>
2023-06-05 16:47:16 +0200

    Simulation

--- blender-hotreload/mesh_converter.py
+++ blender-hotreload/mesh_converter.py
@@ -16,6 +16,8 @@ from bpy.props import (IntProperty,
                        PointerProperty,
                        FloatProperty
                        )
+from bpy_extras.io_utils import ImportHelper
+
 VOXELS_DIR = "export/voxels"
 
 bl_info = {
@@ -219,7 +221,54 @@ class OBJECT_PT_simulation_section(bpy.types.Panel):
 
     def draw_simulation_section(self, context, layout):
         row = layout.row()
-        row.label(text='Simulation', icon='MOD_WAVE')
+        row.label(text='Simulation type', icon='MOD_WAVE')
+        
+        col = layout.column()
+        col.prop(context.scene, 'simulation_types', text="")
+
+        row = layout.column()
+        row.operator('simulation.open_filebrowser', text="Select .obj File", icon='FILEBROWSER')
+        
+        if (context.scene.obj_file_path):
+            row = layout.row()
+            row.label(text=f'Selected file: {context.scene.obj_file_path}')
+        
+        row = layout.row()
+        row.operator('simulation.run_simulation', text="Run Simulation", icon='PLAY')
+
+
+class SIMULATION_OT_open_filebrowser(bpy.types.Operator, ImportHelper):
+    bl_idname = "simulation.open_filebrowser"
+    bl_label = "Select .obj File"
+    filepath = bpy.props.StringProperty(subtype="FILE_PATH")  # Define this to get 'filepath' property to work correctly.
+
+    filter_glob: bpy.props.StringProperty(default="*.obj", options={'HIDDEN'})
+
+    def invoke(self, context, event):
+        context.window_manager.fileselect_add(self)
+        return {'RUNNING_MODAL'}
+
+    def execute(self, context):
+        context.scene.obj_file_path = self.properties.filepath
+        print(f'Selected file: {self.filepath}')
+        return {'FINISHED'}
+
+
+class SIMULATION_OT_execute_simulation(bpy.types.Operator):
+    bl_idname = "simulation.run_simulation"
+    bl_label = "Run Simulation"
+
+    def execute(self, context):
+        if not context.scene.obj_file_path:  # If no file is selected
+            self.report({'WARNING'}, "No .obj file selected. Please select a file to simulate.")
+            return {'CANCELLED'}
+        
+        # Add simulation execution code here
+        print(f'Simulating: {context.scene.obj_file_path}') 
+
+        self.report({'INFO'}, f'Simulation on {os.path.basename(context.scene.obj_file_path)} completed successfully.')
+        return {'FINISHED'}
+
 
 
 
@@ -659,7 +708,9 @@ classes = [
     FilteredObjectItem,
     UpdateListOperator,
     CreateCubeSceneOperator,
-    GlobalSettings
+    GlobalSettings,
+    SIMULATION_OT_open_filebrowser,
+    SIMULATION_OT_execute_simulation
 ]
 
 
@@ -676,6 +727,22 @@ def register():
     bpy.types.Scene.active_object_index = bpy.props.IntProperty(
         update=update_filtered_objects)  # index of active item in list +
     bpy.types.Scene.settings = PointerProperty(type=GlobalSettings)
+    bpy.types.Scene.obj_file_path = bpy.props.StringProperty(
+        name="Obj File Path",
+        description="The path of the selected .obj file for simulation",
+        default="",
+        subtype='FILE_PATH'
+    )
+    bpy.types.Scene.simulation_types = bpy.props.EnumProperty(
+        name="Simulation Types",
+        description="Choose the type of the simulation",
+        items=[
+            ('UNIDIMENSIONAL', "Unidimensional", "Unidimensional simulation"),
+            ('BIDIMENSIONAL', "Bidimensional", "Bidimensional simulation"),
+            ('TRIDIMENSIONAL', "Tridimensional", "Tridimensional simulation"),
+        ]
+    )
+
 
 
 def unregister():
@@ -687,6 +754,9 @@ def unregister():
     del bpy.types.Scene.FilteredObjects
     del bpy.types.Scene.active_object_index
     del bpy.types.Scene.settings
+    del bpy.types.Scene.obj_file_path
+    del bpy.types.Scene.simulation_types
+
 
 
 
